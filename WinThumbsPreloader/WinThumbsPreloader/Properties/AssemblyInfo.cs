bf7acfc6c595dcdf53ff613c6f7a154846efb0f8
Dmitry Bruhov <bruhov@users.noreply.github.com>
2018-02-23 15:53:38 +0500

    Update version number

--- WinThumbsPreloader/WinThumbsPreloader/Properties/AssemblyInfo.cs
+++ WinThumbsPreloader/WinThumbsPreloader/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.1")]
